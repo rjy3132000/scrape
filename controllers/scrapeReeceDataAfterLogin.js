d30d573392b429889657e6f4298d3dd42ef28b39
Rajesh <ry292000@gmail.com>
2024-05-28 18:10:00 +0530

    reeces

--- controllers/scrapeReeceDataAfterLogin.js
+++ controllers/scrapeReeceDataAfterLogin.js
@@ -1,11 +1,12 @@
+const { reeceProductData } = require("../model/productSchema");
 const puppeteer = require("puppeteer");
 const { productRecordsSaveInDB } = require("../utlis/saveProductData");
 
 // Function to login
 async function reeceLogin(username, password, page) {
-  const loginUrl = process.env.ReeceloginUrl || "https://www.reece.com/login";
+  const loginUrl = "https://www.reece.com/login"; //process.env.ReecePageURL; // "https://www.reece.com/login" Replace with the actual login URL if needed ;
   try {
-    await page.goto(loginUrl);
+    await page.goto(loginUrl, { waitUntil: "networkidle2", timeout: 60000 });
     await page.waitForSelector('input[name="email"]');
     await page.type('input[name="email"]', username);
     await page.type('input[name="password"]', password);
@@ -26,145 +27,166 @@ async function reeceLogin(username, password, page) {
   }
 }
 
+// Function to get the product category from URL
+function getProductCategoryFromUrl(url) {
+  const urlParams = new URLSearchParams(new URL(url).search);
+  const categories = urlParams.getAll("categories");
+  return categories.length > 0 ? categories[categories.length - 1] : "unknown";
+}
+
+// Function to scrape a single page
+async function scrapePage(page) {
+  await page.waitForSelector(
+    ".MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42",
+    { visible: true }
+  );
+  // Extracting product details on the current page
+  return await page.evaluate(() => {
+    const products = document.querySelectorAll(
+      ".MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42"
+    );
+    const results = [];
+    products.forEach((product) => {
+      const productName =
+        product
+          .querySelector(".MuiTypography-root.MuiTypography-body1.css-1a0u3kg")
+          ?.textContent.trim() || null;
+      const productStockStirng =
+        product.querySelector(`span[class="pl-1"]`)?.textContent.trim() || 0;
+
+      let productStock = 0;
+      if (productStockStirng != 0) {
+        productStock = +productStockStirng.split(" ")[0];
+      }
+      const priceElement = product.querySelector(
+        'span[class="MuiTypography-root MuiTypography-h4 css-1m2ekip"]'
+      );
+      const price = priceElement?.textContent.trim() || null;
+      const priceUnit = priceElement?.nextSibling?.textContent.trim() || null;
+      const productPrice = price && priceUnit ? `${price} ${priceUnit}` : null;
+      const productImageURL =
+        product.querySelector("img.MuiBox-root.css-4w7ia0")?.src || null;
+      const productBrand =
+        product
+          .querySelector(
+            ".MuiTypography-root.MuiTypography-caption.MuiTypography-gutterBottom.css-kcq2dk"
+          )
+          ?.textContent.trim() || null;
+      const productDetails =
+        product
+          .querySelector(".MuiTypography-root.MuiTypography-body1.css-1a0u3kg")
+          ?.textContent.trim() || null;
+      const productManufactureRefID =
+        product
+          .querySelector(".MuiTypography-root.MuiTypography-caption.css-hnsmw")
+          ?.textContent.trim() || null;
+      const productLink =
+        product
+          .querySelector(
+            "a.MuiTypography-root.MuiTypography-inherit.MuiLink-root.MuiLink-underlineNone.css-116q2oc"
+          )
+          ?.getAttribute("href") || null;
+      let productSku = null;
+      if (productLink && productLink.includes("MSC-")) {
+        const productSKUMatch = productLink.match(
+          /\/product\/[^\/]+\/([^\/]+)/
+        );
+        if (productSKUMatch && productSKUMatch[1].startsWith("MSC-")) {
+          productSku = productSKUMatch[1].substring(4); // Extract SKU after "MSC-"
+        }
+      }
+      results.push({
+        productName,
+        productStock: productStock,
+        productPrice,
+        productImageURL,
+        productBrand,
+        productDetails,
+        productManufactureRefID,
+        productSku,
+      });
+    });
+    return results;
+  });
+}
+
 // Function to scrape search results
-async function scrapeReeceCreateData(baseUrl, username, password) {
+async function scrapeReeceCreateData(baseUrls, username, password) {
   const browserWSEndpoint =
     "https://production-sfo.browserless.io?token=QBR4WvysA0iieKb0bc944a3bbc9fb8ab41b012ec8a";
   const getBrowser = async () => puppeteer.connect({ browserWSEndpoint });
 
   // const browser = await getBrowser();
   const page = await getBrowser().then(async (browser) => browser.newPage());
+  let allData = [];
+
   try {
     // Perform login
     await reeceLogin(username, password, page);
     // Wait for navigation to complete
     await page.waitForNavigation();
-    // Navigate to the search results page
-    await page.goto(baseUrl, { waitUntil: "domcontentloaded" });
-    await page.waitForSelector('div[data-testid="pagination"]');
-    // Get the total number of pages
-    const totalPages = await page.evaluate(() => {
-      return parseInt(
-        document
-          .querySelector('span[data-testid="pagination-total"]')
-          .textContent.trim(),
-        10
-      );
-    });
-    let data = [];
-    for (let i = 1; i <= totalPages; i++) {
-      console.log(`Scraping page ${i} of ${totalPages}`);
-      try {
-        await page.waitForNavigation({ waitUntil: "domcontentloaded" });
-      } catch (error) {
-        console.error("Navigation timeout error:");
-      }
-      await page.waitForSelector(
-        ".MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42",
-        { visible: true }
-      );
-      // Extracting product details on the current page
-      const pageData = await page.evaluate(() => {
-        const products = document.querySelectorAll(
-          ".MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42"
+
+    for (const baseUrl of baseUrls) {
+      console.log(`Navigating to URL: ${baseUrl}`);
+      // Navigate to the search results page
+      await page.goto(baseUrl, { waitUntil: "domcontentloaded" });
+      await page.waitForSelector('div[data-testid="pagination"]');
+
+      // Get the total number of pages
+      const totalPages = await page.evaluate(() => {
+        return parseInt(
+          document
+            .querySelector('span[data-testid="pagination-total"]')
+            .textContent.trim(),
+          10
         );
-        const results = [];
-        products.forEach((product) => {
-          const productName =
-            product
-              .querySelector(
-                ".MuiTypography-root.MuiTypography-body1.css-1a0u3kg"
-              )
-              ?.textContent.trim() || null;
-          const productStockStirng =
-            product.querySelector(`span[class="pl-1"]`)?.textContent.trim() ||
-            0;
-
-          let productStock = 0;
-          if (productStockStirng != 0) {
-            productStock = +productStockStirng.split(" ")[0];
-          }
-          const priceElement = product.querySelector(
-            'span[class="MuiTypography-root MuiTypography-h4 css-1m2ekip"]'
-          );
-          const price = priceElement?.textContent.trim() || null;
-          const priceUnit =
-            priceElement?.nextSibling?.textContent.trim() || null;
-          const productPrice =
-            price && priceUnit ? `${price} ${priceUnit}` : null;
-          const productImageURL =
-            product.querySelector("img.MuiBox-root.css-4w7ia0")?.src || null;
-          const productBrand =
-            product
-              .querySelector(
-                ".MuiTypography-root.MuiTypography-caption.MuiTypography-gutterBottom.css-kcq2dk"
-              )
-              ?.textContent.trim() || null;
-          const productDetails =
-            product
-              .querySelector(
-                ".MuiTypography-root.MuiTypography-body1.css-1a0u3kg"
-              )
-              ?.textContent.trim() || null;
-          const productManufactureRefID =
-            product
-              .querySelector(
-                ".MuiTypography-root.MuiTypography-caption.css-hnsmw"
-              )
-              ?.textContent.trim() || null;
-          const productLink =
-            product
-              .querySelector(
-                "a.MuiTypography-root.MuiTypography-inherit.MuiLink-root.MuiLink-underlineNone.css-116q2oc"
-              )
-              ?.getAttribute("href") || null;
-          let productSku = "";
-          if (productLink && productLink.includes("MSC-")) {
-            const productSKUMatch = productLink.match(
-              /\/product\/[^\/]+\/([^\/]+)/
-            );
-            if (productSKUMatch && productSKUMatch[1].startsWith("MSC-")) {
-              productSku = productSKUMatch[1].substring(4); // Extract SKU after "MSC-"
-            }
-          }
-
-          results.push({
-            productName,
-            productStock,
-            productPrice,
-            productImageURL,
-            productBrand,
-            productDetails,
-            productManufactureRefID,
-            productSku,
-          });
-        });
-        return results;
       });
-      // Add default values
-      const enrichedPageData = pageData.map((item) => ({
-        ...item,
-        productCategory: "water heater",
-        productSupplier: "Reece",
-        productStatus: true,
-      }));
-      data = data.concat(enrichedPageData);
-      // Check if there is a next page
-      if (i < totalPages) {
+
+      // Get the product category from URL
+      const productCategory = getProductCategoryFromUrl(baseUrl);
+
+      let data = [];
+      for (let i = 1; i <= totalPages; i++) {
+        console.log(`Scraping page ${i} of ${totalPages}`);
         try {
-          await Promise.all([
-            page.click('button[data-testid="pagination-next"]'),
-            page.waitForSelector(
-              ".MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42",
-              { visible: true }
-            ),
-          ]);
+          await page.waitForNavigation({ waitUntil: "domcontentloaded" });
         } catch (error) {
-          console.error(`Failed to navigate to page ${i + 1}:`, error);
+          // console.error("Navigation timeout error:");
+        }
+        await page.waitForSelector(
+          ".MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42",
+          { visible: true }
+        );
+        const pageData = await scrapePage(page);
+
+        // Add default values
+        const enrichedPageData = pageData.map((item) => ({
+          ...item,
+          productCategory: productCategory,
+          productSupplier: "Reece",
+          productStatus: true,
+        }));
+        data = data.concat(enrichedPageData);
+
+        // Check if there is a next page
+        if (i < totalPages) {
+          try {
+            await Promise.all([
+              page.click('button[data-testid="pagination-next"]'),
+              page.waitForSelector(
+                ".MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42",
+                { visible: true }
+              ),
+            ]);
+          } catch (error) {
+            console.error(`Failed to navigate to page ${i + 1}:`, error);
+          }
         }
       }
+      allData = allData.concat(data);
     }
-    return data;
+
+    return allData;
   } catch (error) {
     console.log("Error: ", error);
     return [];
@@ -175,17 +197,32 @@ async function scrapeReeceCreateData(baseUrl, username, password) {
 
 // Example usage
 const scrapeReeceDataAfterLogin = async () => {
-  const baseUrl =
-    process.env.ReeceBaseURL ||
-    "https://www.reece.com/search?&categories=Water%20Heaters&categories=Commercial%20-%20Electric";
-  const username = process.env.ReeceUserName || "austin@callnublue.com";
-  const password = process.env.ReecePassword || "BlueBuy1!";
+  const baseUrls = [
+    "https://www.reece.com/search?&categories=Water%20Heaters&categories=Residential%20-%20Electric",
+    // "https://www.reece.com/search?&categories=Water%20Heaters&categories=Residential%20-%20Gas",
+    // "https://www.reece.com/search?&categories=Water%20Heaters&categories=Commercial%20-%20Electric",
+    // "https://www.reece.com/search?&categories=Water%20Heaters&categories=Commercial%20-%20Tankless",
+    // "https://www.reece.com/search?&categories=Water%20Heaters&categories=Residential%20-%20Tankless",
+    // "https://www.reece.com/search?&categories=Water%20Heaters&categories=Commercial%20-%20Gas",
+    // "https://www.reece.com/search?&categories=Water%20Heaters&categories=Water%20Heater%20Parts%20%26%20Accessories&categories=Connectors",
+    // "https://www.reece.com/search?&categories=Water%20Heaters&categories=Water%20Heater%20Parts%20%26%20Accessories&categories=Earthquake%20Straps",
+    // "https://www.reece.com/search?&categories=Water%20Heaters&categories=Water%20Heater%20Parts%20%26%20Accessories&categories=Expansion%20Tank",
+    // "https://www.reece.com/search?&categories=Water%20Heaters&categories=Water%20Heater%20Parts%20%26%20Accessories&categories=Pan",
+    // "https://www.reece.com/search?&categories=Water%20Heaters&categories=Water%20Heater%20Parts%20%26%20Accessories&categories=Parts",
+    // "https://www.reece.com/search?&categories=Water%20Heaters&categories=Water%20Heater%20Parts%20%26%20Accessories&categories=Tankless%20Valves",
+    // "https://www.reece.com/search?&categories=Water%20Heaters&categories=Water%20Heater%20Parts%20%26%20Accessories&categories=Other",
+  ];
+  const username = "austin@callnublue.com";
+  const password = "BlueBuy1!";
   try {
     const searchResults = await scrapeReeceCreateData(
-      baseUrl,
+      baseUrls,
       username,
       password
     );
+    //await WaterHeater.insertMany(searchResults);
+    console.log("Data saved to MongoDB successfully.");
+    console.log("searchResults=============", searchResults[0]);
     return searchResults;
   } catch (error) {
     console.error("Error scraping data: ", error);
@@ -196,14 +233,15 @@ const scrapeReeceDataAfterLogin = async () => {
 const scrapeReeceData = async (req, res) => {
   try {
     const scrapData = await scrapeReeceDataAfterLogin();
+    console.log("data length------------", scrapData.length); //reece
     const saveData = await productRecordsSaveInDB("reece", scrapData);
     if (saveData) {
-      res.status(200).send(saveData);
+      res.status(200).json(saveData);
     } else {
-      res.status(400).send({ message: "Data is not saved" });
+      res.status(400).json({ message: "Data is not saved" });
     }
   } catch (error) {
-    console.error("Error in /scrape-hughes-data route:", error);
+    console.error("Error in /scrape-reece-data route:", error);
     res.status(500).send("Error scraping data.");
   }
 };
